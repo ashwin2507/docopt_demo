6ed8e26f6acc432c7ad47cd1ed66e0e5db3b4556
Ashwin Babu <ash2507@student.ubc.ca>
2022-11-19 12:41:14 -0800

    Added main function

--- src/demo.R
+++ src/demo.R
@@ -6,11 +6,15 @@ Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>]
 
 Options:
 <arg>             Takes any value (this is a required positional argument)
+[<arg_new>]	  Takes any value (this is an optional positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
 " -> doc
+main <- function{
+	opt <- docopt(doc)
+	print(opt)
+	print(typeof(opt))
+}
 
-library(docopt)
-opt <- docopt(doc)
-print(opt)
-print(typeof(opt))
+
+main()
